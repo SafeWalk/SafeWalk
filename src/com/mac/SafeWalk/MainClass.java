5e2240d6b96af4ec20d3738d3c5daaf2f2172b38
Kohei Hisakuni <khisakun@macalester.edu>
2014-02-24 12:50:16 -0600

    Added TextEdit

--- src/com/mac/SafeWalk/MainClass.java
+++ src/com/mac/SafeWalk/MainClass.java
@@ -4,20 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.*;
 
 /**
  *
  */
-public class MainClass extends Activity {
+public class MainClass extends Activity implements AdapterView.OnItemSelectedListener {
 
     /*
      * Called when the activity is first created
      */
     private String pickUpLocation;
-    public final static String EXTRA_MESSAGE = "com.mac.SafeWalk.MESSAGE";
+    private String customPickUpLocation;
+    private boolean isCustom = false;
+    public final static String SPINNER_LOCATION = "com.mac.SafeWalk.SPINNER_MESSAGE";
+    public final static String CUSTOM_LOCATION = "com.mac.SafeWalk.CUSTOM_MESSAGE";
+    public final static String IS_CUSTOM = "com.mac.SafeWalk.IS_CUSTOM";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,22 +27,47 @@ public class MainClass extends Activity {
         setContentView(R.layout.main);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.pick_up_choices,
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.pick_up_choices,
                 android.R.layout.simple_spinner_item);
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinner.setAdapter(adapter);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 
-    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        pickUpLocation = (String) parent.getItemAtPosition(pos);
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+    {
+        pickUpLocation = parent.getItemAtPosition(pos).toString();
     }
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+        EditText customEdit = (EditText)findViewById(R.id.customLocationText);
+        pickUpLocation = customEdit.getText().toString();
+
+    }
+
 
     /**
      * Called when the user clicks the send button.
      */
     public void sendClick(View view) {
+
         Intent intent = new Intent(this, SendMessageToSafeWalk.class);
-        intent.putExtra(EXTRA_MESSAGE, pickUpLocation);
+        //Bundle extras = new Bundle();
+        //extras.putString(SPINNER_LOCATION, pickUpLocation);
+        // Retrieve address if user entered it.
+//        EditText customEdit = (EditText)findViewById(R.id.customLocationText);
+//        if (customEdit.getText().toString() != null)
+//        {
+//            isCustom = true;
+//            customPickUpLocation = customEdit.getText().toString();
+//            extras.putString(CUSTOM_LOCATION, customPickUpLocation);
+//        };
+        //extras.putBoolean(IS_CUSTOM, isCustom);
+        //intent.putExtras(extras);
+        intent.putExtra(CUSTOM_LOCATION, pickUpLocation);
         startActivity(intent);
     }
 
--- src/com/mac/SafeWalk/SendMessageToSafeWalk.java
+++ src/com/mac/SafeWalk/SendMessageToSafeWalk.java
@@ -1,25 +1,60 @@
 package com.mac.SafeWalk;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.widget.TextView;
+
+import static com.mac.SafeWalk.MainClass.*;
 
 /**
  * This activity should send the SMS to a predetermined SafeWalk number
  */
 public class SendMessageToSafeWalk extends Activity {
 
+    private boolean isCustom = false;
+    private String location;
+    public final static String PHONE_NUMBER = "6512420083";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Intent intent = getIntent();
-        String location = intent.getStringExtra(MainClass.EXTRA_MESSAGE);
+        location = intent.getStringExtra(CUSTOM_LOCATION);
+
+
+        //Bundle extras = getIntent().getExtras();
+
+//        isCustom = extras.getBoolean(MainClass.IS_CUSTOM);
+//        if (isCustom == true)
+//        {
+//            location = extras.getString(MainClass.CUSTOM_LOCATION);
+//        } else {
+//            location = extras.getString(MainClass.SPINNER_LOCATION);
+//        }
 
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(location);
 
         setContentView(textView);
+
+
+
+        try
+        {
+            SmsManager.getDefault().sendTextMessage(PHONE_NUMBER, null, location, null, null);
+        }
+        catch (Exception e)
+        {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            AlertDialog dialog = alertDialogBuilder.create();
+
+            dialog.setMessage(e.getMessage());
+            dialog.show();
+        }
     }
 }
