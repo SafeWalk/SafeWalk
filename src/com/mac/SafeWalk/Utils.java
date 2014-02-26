f46c48c79b8769e5a6decdc8ca6b662a548bd2cd
Guillermo Vera <guillermovera92@gmail.com>
2014-02-26 17:06:04 -0600

    Some code clean up and decomposition. Added Utils class to hold global variables.

---
+++ src/com/mac/SafeWalk/Utils.java
@@ -0,0 +1,32 @@
+package com.mac.SafeWalk;
+
+/**
+ * Utilities class that keeps track of global variables. It is a singleton so there
+ * is only one instance of this class.
+ */
+public class Utils {
+
+    private static final Utils UTILS = new Utils();
+
+    private String pickUpLocation;
+    private final static String SAFEWALK_PHONE_NUMBER = "6512420083";  //Currently Kohei's number
+
+
+    private void Utils() {}
+
+    public static Utils getUtils() {
+        return UTILS;
+    }
+
+    public String getPickUpLocation() {
+        return pickUpLocation;
+    }
+
+    public void setPickUpLocation(String pickUpLocation) {
+        this.pickUpLocation = pickUpLocation;
+    }
+
+    public static String getSafewalkPhoneNumber() {
+        return SAFEWALK_PHONE_NUMBER;
+    }
+}
