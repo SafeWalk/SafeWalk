6a6635b8fa1d1b50dfbb9d13f8b5a355f6a0b681
Guillermo Vera <guillermovera92@gmail.com>
2014-02-26 17:03:56 -0600

    Some code clean up and decomposition. Added Utils class to hold global variables.

--- src/com/mac/SafeWalk/MainClass.java
+++ src/com/mac/SafeWalk/MainClass.java
@@ -13,13 +13,8 @@ import android.widget.*;
 public class MainClass extends Activity {
 
 
-    // The location gets stored here.
-    private String pickUpLocation;
     // Boolean to check if student is choosing from spinner or inputting address.
     private boolean isCustom;
-    public final static String LOCATION = "com.mac.SafeWalk.LOCATION";
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,16 +22,59 @@ public class MainClass extends Activity {
         setContentView(R.layout.main);
 
         // set up spinner
-        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner spinner = setSpinner();
 
+        onSelectedInSpinner(spinner);
+    }
+
+    /**
+     * Retrieve address typed in the Edit Text that appears when "Other" is selected on the spinner
+     * @param editText Edit Text field of the UI
+     * @return String of the typed address
+     */
+    private String retrieveLocation(EditText editText)
+    {
+        return editText.getText().toString();
+    }
+
+    /**
+     * Sends the pick-up location to the next activity so that the Sms Manager can send a text message.
+     * Accessed when "send" button is clicked. Retrieves data from EditText via retrieveLocation method.
+     */
+    public void sendClick(View view) {
+        EditText customEdit = (EditText)findViewById(R.id.customLocationText);
+        if (isCustom) {
+            Utils.getUtils().setPickUpLocation(retrieveLocation(customEdit));
+        }
+        Intent intent = new Intent(this, SendMessageToSafeWalk.class);
+        startActivity(intent);
+
+    }
+
+    /**
+     * This method sets up the spinner wheel for the different pickup choices of the main UI
+     * @return spinner
+     */
+    private Spinner setSpinner(){
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.pick_up_choices,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        return spinner;
+    }
+
+    /**
+     * Sets the pick up location of the variable pickUpLocation on the Utils class depending on the
+     * selection
+     * @param spinner Spinner with all options and "Other" option
+     */
+    private void onSelectedInSpinner(final Spinner spinner){
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
@@ -48,7 +86,7 @@ public class MainClass extends Activity {
                     isCustom = true;
                 } else {
                     // Otherwise the pick-up location is whatever the user chooses from spinner.
-                    pickUpLocation = parent.getItemAtPosition(position).toString();
+                    Utils.getUtils().setPickUpLocation(parent.getItemAtPosition(position).toString());
                 }
             }
 
@@ -59,36 +97,6 @@ public class MainClass extends Activity {
             }
         });
 
-
     }
-
-    // Retrieves location string from EditText.
-    public String retrieveLocation(EditText editText)
-    {
-        String location;
-        location = editText.getText().toString();
-        return location;
-    }
-
-    /*
-    Sends the pick-up location to the next activity so that the Sms Manager can send a text message.
-    Accessed when "send" button is clicked. Retrieves data from EditText via retrieveLocation method.
-     */
-    public void sendClick(View view)
-    {
-
-        EditText customEdit = (EditText)findViewById(R.id.customLocationText);
-        if (isCustom == true)
-        {
-            pickUpLocation = retrieveLocation(customEdit);
-        }
-
-        Intent intent = new Intent(this, SendMessageToSafeWalk.class);
-        intent.putExtra(LOCATION, pickUpLocation);
-        startActivity(intent);
-
-    }
-
-
 
 }
--- src/com/mac/SafeWalk/SendMessageToSafeWalk.java
+++ src/com/mac/SafeWalk/SendMessageToSafeWalk.java
@@ -14,41 +14,28 @@ import static com.mac.SafeWalk.MainClass.*;
  */
 public class SendMessageToSafeWalk extends Activity {
 
-
-    // This is the pick-up location
-    private String location;
-    // This is the predetermined SafeWalk number. Currently it is Kohei's number.
-    public final static String PHONE_NUMBER = "6512420083";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Intent intent = getIntent();
-
-        // Gets the pick-up location from previous activity.
-        location = intent.getStringExtra(LOCATION);
 
         // Displays pick-up location.
         TextView textView = new TextView(this);
         textView.setTextSize(40);
-        textView.setText(location);
+        textView.setText(Utils.getUtils().getPickUpLocation());
         setContentView(textView);
 
         // Sends SMS to provided phone number.
-        sendSms(PHONE_NUMBER);
+        sendSms(Utils.getSafewalkPhoneNumber());
     }
-
-
 
     /*
     Sends SMS to a phone number. If invalid number, a dialog informs the user.
      */
-    public void sendSms(String phoneNumber)
-    {
+    public void sendSms(String phoneNumber) {
         try
         {
-            SmsManager.getDefault().sendTextMessage(phoneNumber, null, location, null, null);
+            SmsManager.getDefault().sendTextMessage(phoneNumber, null, Utils.getUtils().getPickUpLocation(), null, null);
         }
         catch (Exception e)
         {
