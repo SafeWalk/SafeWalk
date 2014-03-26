dc858e8646802705f2a9d040486d7574403db71d
Rhyan <rfookune@macalester.edu>
2014-03-25 19:50:32 -0500

    SettingsActivity is launched if no personal info are stored. Goes directly to HomeScreenActivity when settings are saved.

--- src/com/mac/SafeWalk/SettingsActivity.java
+++ src/com/mac/SafeWalk/SettingsActivity.java
@@ -1,21 +1,20 @@
 package com.mac.SafeWalk;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class SettingsActivity extends Activity implements View.OnClickListener {
 
     private EditText name;
     private EditText phone;
-    private TextView nameResult;
-    private TextView phoneResult;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,10 +51,35 @@ public class SettingsActivity extends Activity implements View.OnClickListener {
         SharedPreferences.Editor nameEditor = Settings.getSettings().getNameData().edit();
         nameEditor.putString("sharedName", stringData1);
         nameEditor.commit();
-        //  Save phone number
+
+        //  Save phone number (if valid number)
+        // need to add no more then 10 in length
         String stringData2 = phone.getText().toString();
-        SharedPreferences.Editor phoneEditor = Settings.getSettings().getPhoneData().edit();
-        phoneEditor.putString("sharedPhone", stringData2);
-        phoneEditor.commit();
+        if ((verifyInt(stringData2))){
+            SharedPreferences.Editor phoneEditor = Settings.getSettings().getPhoneData().edit();
+            phoneEditor.putString("sharedPhone", stringData2);
+            phoneEditor.commit();
+
+//            Go back to main activity if the data is saved
+            Intent homeScreen = new Intent(this, HomeScreenActivity.class);
+            startActivity(homeScreen);
+//            Display a message
+            Toast.makeText(getApplicationContext(), "Your settings have been successfully saved", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Invalid phone number", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    //    Verify if the input is a integer
+    public boolean verifyInt(String s){
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
     }
 }
